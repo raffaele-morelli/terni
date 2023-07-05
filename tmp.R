e632517c67cf7f9e77990d3ca579eef643f21eaf
raffaele-morelli <raffaele.morelli@isprambiente.it>
2023-07-05 10:04:38 +0200

    file temporaneo per appunti ed eventuali

---
+++ tmp.R
@@ -0,0 +1,13 @@
+## esempio di plot 
+# ggplot() + 
+#   # geom_sf(data = com) +
+#   # geom_sf(data = filter(terni_utm32, code_2018 == 12210)) +
+#   geom_sf(data = filter(terni_utm32, code_2018 == 12220), color = alpha("grey", 0.2)) +
+#   geom_raster(data = imper_rst, aes(x = x, y = y, fill = impermeabilizzazione_utm32), alpha = 0.5) +
+#   scale_fill_viridis_c() +
+#   geom_sf(data = st_buffer(pt_misura, 200), color = "green") +
+#   geom_sf(data = pt_misura, color = "red", size = 0.5) +
+#   geom_sf(data = i_12220, fill = "violet") +
+#   coord_sf(xlim = c(794643.0, 801954.6), ylim = c(4717144.4, 4720794.3)) +
+#   theme_void()
+
