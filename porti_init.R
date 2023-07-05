ec153ba5c96d918eb407e6f83f0e1c2cec935990
raffaele-morelli <raffaele.morelli@isprambiente.it>
2023-07-05 10:28:57 +0200

    fix blocchi

--- porti_init.R
+++ porti_init.R
@@ -11,7 +11,7 @@
   terni_all <- st_read("/home/rmorelli/R/porti/data/IT515L2_TERNI_UA2018_v013/Data/IT515L2_TERNI_UA2018_v013.gpkg")
   # com <- st_read("/home/rmorelli/R/porti/Limiti01012021/Com01012021/Com01012021_WGS84.shp") %>% filter(PRO_COM == 55032) # limiti comunali
   
-  pt_misura <- st_read("data/punti_misura.shp")
+  pt_misura <- st_read("/home/rmorelli/R/porti/data/punti_misura.shp")
   
   # le variabil di interesse
   terni_fltr <- filter(terni_all, code_2018 %in% c(11100, 11210, 11220, 11230, 11240, 12100, 12210, 12220))
