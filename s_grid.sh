6c80cd7d90019d3a5b5c5c810e62f59346f8f452
raffaele-morelli <raffaele.morelli@isprambiente.it>
2023-12-28 11:58:56 +0100

    update

--- s_grid.sh
+++ s_grid.sh
@@ -1,26 +1,95 @@
 #!/bin/sh
 
 # traccianti ####
+Rscript f_grid.R Al_i &
+Rscript f_grid.R Al_s &
+Rscript f_grid.R As_i &
+Rscript f_grid.R As_s &
+Rscript f_grid.R B_i &
+Rscript f_grid.R B_s &
+Rscript f_grid.R Ba_i &
+Rscript f_grid.R Ba_s &
+wait
+
+Rscript f_grid.R Bi_i &
+Rscript f_grid.R Bi_s &
+Rscript f_grid.R Ca_i &
+Rscript f_grid.R Ca_s &
+Rscript f_grid.R Cd_i &
+Rscript f_grid.R Cd_s &
+Rscript f_grid.R Ce_i &
+wait
+
+Rscript f_grid.R Ce_s &
+Rscript f_grid.R Co_i &
+Rscript f_grid.R Co_s &
 Rscript f_grid.R Cr_i &
-Rscript f_grid.R Mo_s &
-Rscript f_grid.R Ni_i &
-wait
-
+Rscript f_grid.R Cr_s &
+Rscript f_grid.R Cs_i &
 Rscript f_grid.R Cs_s &
-Rscript f_grid.R Rb_i &
-Rscript f_grid.R U_i &
-wait
-
 Rscript f_grid.R Cu_i &
-Rscript f_grid.R Sb_i &
-Rscript f_grid.R Sn_i &
 wait
 
-Rscript f_grid.R Cs_s &
+Rscript f_grid.R Cu_s &
+Rscript f_grid.R Fe_i &
+Rscript f_grid.R Fe_s &
+Rscript f_grid.R Ga_i &
+Rscript f_grid.R Ga_s &
+Rscript f_grid.R K_i &
 Rscript f_grid.R K_s &
-Rscript f_grid.R Rb_s &
+Rscript f_grid.R La_i &
 wait
 
-Rscript f_grid.R Tl_s &
-Rscript f_grid.R Rb_s &
+Rscript f_grid.R La_s &
+Rscript f_grid.R Li_i &
+Rscript f_grid.R Li_s &
+Rscript f_grid.R Mg_i &
+Rscript f_grid.R Mg_s &
+Rscript f_grid.R Mn_i &
+Rscript f_grid.R Mn_s &
+Rscript f_grid.R Mo_i &
 wait
+
+Rscript f_grid.R Mo_s &
+Rscript f_grid.R Na_i &
+Rscript f_grid.R Na_s &
+Rscript f_grid.R Nb_i &
+Rscript f_grid.R Nb_s &
+Rscript f_grid.R Ni_i &
+Rscript f_grid.R Ni_s &
+Rscript f_grid.R PM10 &
+wait
+
+Rscript f_grid.R Pb_i &
+Rscript f_grid.R Pb_s &
+Rscript f_grid.R Rb_i &
+Rscript f_grid.R Rb_s &
+Rscript f_grid.R Sb_i &
+Rscript f_grid.R Sb_s &
+Rscript f_grid.R Sn_i &
+Rscript f_grid.R Sn_s &
+wait
+
+Rscript f_grid.R Sr_i &
+Rscript f_grid.R Sr_s &
+Rscript f_grid.R Ti_i &
+Rscript f_grid.R Ti_s &
+Rscript f_grid.R Tl_i &
+Rscript f_grid.R Tl_s &
+Rscript f_grid.R U_i &
+Rscript f_grid.R U_s &
+wait
+
+Rscript f_grid.R V_i &
+Rscript f_grid.R V_s &
+Rscript f_grid.R W_i &
+Rscript f_grid.R W_s &
+Rscript f_grid.R Zn_i &
+Rscript f_grid.R Zn_s &
+Rscript f_grid.R Zr_i &
+Rscript f_grid.R Zr_s &
+wait
+
+# ## INDICI ####
+# Rscript f_grid.R TOT_CR &
+# Rscript f_grid.R TOT_NCR &
