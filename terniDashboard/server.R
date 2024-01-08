326d8fb5cbac40420e17720d9a2e28f20a93e82d
raffaele-morelli <raffaele.morelli@isprambiente.it>
2024-01-08 12:16:09 +0100

    legenda

--- terniDashboard/server.R
+++ terniDashboard/server.R
@@ -85,27 +85,5 @@ server <- function(input, output) {
     
   }, width = 1200, height = 600)
   
-  output$plot <- renderPlotly({
-    if (is.null(input$traccianti)) {return(NULL)}
-    
-    f <- glue::glue('/home/rmorelli/R/terni/rds_out_traccianti/{input$traccianti}_{input$res}m_{input$res}res.RDS')
-    
-    trcnt <- readRDS(f)
-    trcnt_df <- do.call(rbind.data.frame, trcnt)
-    
-    if(input$res == 100) {
-      n_col <- 109
-    }else{
-      n_col <- 54
-    }
-    
-    r <- matrix(trcnt_df[, 1], ncol = n_col,  byrow = FALSE) %>% raster::raster()
-    raster::extent(r) <- r_extent
-    crs(r) <- " +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-    
-    r_df <- as.data.frame(r, xy = TRUE) %>% na.omit() %>% setNames(c("x", "y", "value"))
-    ggplot(data = r_df) +
-      geom_raster(aes(x = x, y = y, fill = value)) 
-    # plot_ly(data = r_df) %>% add_rasterly_image(x = ~x, y = ~y, color = ~z)
-  })
+
 }
