dec8fe333cf41d2f89a011daca56c8ad358e2248
raffaele-morelli <raffaele.morelli@isprambiente.it>
2024-01-08 06:50:55 +0100

    dashboard

--- f_tiff.R
+++ f_tiff.R
@@ -1,6 +1,8 @@
 library(sf)
 library(glue)
 library(stringr)
+library(terra)
+library(ggplot2)
 
 # pltnt <- "Cr_i"
 for (i in seq(1:12)) {
@@ -9,7 +11,8 @@ for (i in seq(1:12)) {
 }
 
 mese <- 1
-ext <- ""
+res <- 200
+grd <- 200
 
 {
   dominio <- st_read(glue("~/R/terni/data/dominio/dominio_200m.shp")) # 54 col
@@ -19,24 +22,40 @@ ext <- ""
   # dominio <- st_read("~/R/terni/data/dominio/dominio_100m_redux.shp") # 76 col
 }
 
+terni_sez <- st_read("~/R/terni/data/shp/Terni_sez.shp") # sezioni di censimento
+
 pt_misura_utm32 <- st_read("~/R/terni/data/shp/punti_misura.shp")
+
 st_bbox(dominio) -> bbox
 r_extent <- c(as.numeric(bbox["xmin"]), as.numeric(bbox["xmax"]), as.numeric(bbox["ymin"]), as.numeric(bbox["ymax"]))
 
-fls <- list.files("~/R/terni/rds_out_traccianti/", pattern = "200m_100res", full.names = TRUE)
+fls <- list.files("~/R/terni/rds_out_traccianti", pattern = glue("{grd}m_{res}res"), full.names = TRUE)
 
 purrr::walk(fls, \(f) {
   tools::file_path_sans_ext(f) %>% basename() -> fout
   print(fout)
-  
+
   trcnt <- readRDS(f)
   trcnt_df <- do.call(rbind.data.frame, trcnt)
-  
-  r <- matrix(trcnt_df[,mese], ncol = 109,  byrow = FALSE) %>% raster::raster()
+
+  r <- matrix(trcnt_df[,mese], ncol = 54,  byrow = FALSE) %>% raster::raster()
   
   mese <- str_pad(mese, 2, pad = "0")
   raster::extent(r) <- r_extent
-  r <- terra::writeRaster(r, glue::glue('~/R/terni/tiff_out/{mese}/{fout}_{mese}.tif'), overwrite = TRUE)
+  crs(r) <- "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs"
+  
+  terra::writeRaster(r, glue::glue('~/R/terni/tiff_out/{mese}/{fout}_{mese}.tif'), overwrite = TRUE)
+  r_df <- as.data.frame(r, xy = TRUE) %>% na.omit() %>% setNames(c("x", "y", "value"))
+  
+  ggplot(data = r_df) +
+    geom_raster(aes(x = x, y = y, fill = value)) +
+    geom_sf(data = st_crop(terni_sez, st_bbox(r)), color = "grey90", fill = "transparent", size = 0.5) +
+    geom_sf(data = pt_misura_utm32, shape = 21, fill = "lightgray", color = "black", size = 3) +
+    scale_fill_viridis_c(direction = -1, option = "magma") +
+    theme_void() +
+    theme(legend.position = "none") + 
+    coord_sf(datum = sf::st_crs(32632)) -> g
+  
+  ggsave(filename = glue::glue('~/R/terni/png_out/{fout}_{mese}.png'), plot = g, 
+         width = 1000, height = 600, units = "px", dpi = 72)
 })
-
-
--- terniApp/app.R
+++ terniApp/app.R
@@ -5,91 +5,126 @@ library(ggplot2)
 library(summarytools)
 library(knitr)
 library(kableExtra)
+library(sf)
+library(glue)
+library(terra)
 
-df <- readr::read_csv("/home/rmorelli/R/terni/data/dataframes/df_finale_lod_clean.csv", show_col_types = FALSE)
-models <- readRDS("/home/rmorelli/R/terni/rds_out/modelli_all_clean.RDS")
-crssvld <- readRDS("/home/rmorelli/R/terni/rds_out/cross_validation_all.RDS")
 
-pltnts <-  readRDS("/home/rmorelli/R/terni/traccianti.RDS")
+df <- readr::read_csv("/home/rmorelli/R/terni/data/dataframes/df_finale_raw.csv", show_col_types = FALSE)
+models <- readRDS("/home/rmorelli/R/terni/rds_out/modelli_gaussian_clean.RDS")
+# crssvld <- readRDS("/home/rmorelli/R/terni/rds_out/cross_validation_gaussian.RDS")
+
+# terni_sez <- st_read("~/R/terni/data/shp/Terni_sez.shp") # sezioni di censimento
+
+dominio <- st_read(glue("/home/rmorelli/R/terni/data/dominio/dominio_200m.shp")) # 54 col
+st_bbox(dominio) -> bbox
+r_extent <- c(as.numeric(bbox["xmin"]), as.numeric(bbox["xmax"]), as.numeric(bbox["ymin"]), as.numeric(bbox["ymax"]))
+rm(dominio)
+
+pltnts <-  readRDS("/home/rmorelli/R/terni/rds_out/traccianti.RDS")
 
 ui <- fluidPage(
-  titlePanel("Esercizio Terni"),
+  # titlePanel("Esercizio Terni"),
   
   # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 10,
-                  max = 60,
-                  value = 40),
-      selectInput(
-        "traccianti", "Tracciante", choices = c(
-          "All", pltnts
-        )
-      ),
-      # selectInput(
-      #   "set", "Set di variabili", choices = c(
-      #     "All", "Scelte", "Mean"
-      #   )
-      # )            
-    ),
+  # sidebarLayout(
+  #   sidebarPanel(
+  #     # sliderInput("bins", "Number of bins:",
+  #     #             min = 10, max = 60,
+  #     #             value = 40),
+  #     selectInput(
+  #       "traccianti", "Tracciante", choices = c(
+  #         "All", pltnts
+  #       )
+  #     ),
+  #     selectInput(
+  #       "res", "Risoluzione", choices = c(
+  #         "200", "100"
+  #       )
+  #     ),
+  #     plotOutput("distPlot", width = "100%", height = "300px")
+  #     
+  #   ),
+
     # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot", width = "75%", height = "400px"),
-      verbatimTextOutput("summary"),
-      plotOutput("check", width = "100%", height = "600px"),
-      plotOutput("splines", width = "100%", height = "800px"),
-      # textOutput("crossvld"),
-    )
-  )
+    # mainPanel(
+      fluidRow(
+        column(4, 
+               selectInput("traccianti", "Tracciante", choices = c(pltnts)),
+               selectInput("res", "Risoluzione", choices = c("100", "200")),
+               plotOutput("distPlot", width = "100%", height = "300px"),
+        ),
+        column(4, verbatimTextOutput("summary")),
+        column(4, plotOutput("check"))
+        # verbatimTextOutput("summary"),
+        # plotOutput("check", width = "500px", height = "500px"),
+        # plotOutput("splines", width = "75%", height = "500px"),
+        # plotOutput("predict", width = "75%")
+      ),
+      fluidRow(
+        column(4, 
+               plotOutput("splines")
+        ),
+        column(6, 
+               plotOutput("predict", width = "70%")
+        )
+      )
+    # )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
   output$distPlot <- renderPlot({
-    if(input$traccianti == "All") { 
+    if(input$traccianti == "All") {
       return("Seleziona un tracciante")
     }
     df <- data.frame(log(df[[input$traccianti]]))
-    
-    reshape2::melt(df) %>% 
-      ggplot(aes(x = value) ) + 
-      geom_histogram(bins = input$bins, fill = 'dodgerblue4', colour = 'white') + xlab("") + ylab("") +
+
+    reshape2::melt(df) %>%
+      ggplot(aes(x = value) ) +
+      geom_histogram(bins = 60, fill = 'dodgerblue4', colour = 'white') + xlab("") + ylab("") +
       ggtitle(input$traccianti)
-    
+
   })
   
   output$splines <- renderPlot({
-    if(input$traccianti == "All") { 
-      return("Seleziona un tracciante") 
+    if(input$traccianti == "All") {
+      return("Seleziona un tracciante")
     }
     gratia::draw(models[[input$traccianti]])
   })
-  
+
   output$check <- renderPlot({
-    if(input$traccianti == "All") { 
+    if(input$traccianti == "All") {
       return("Seleziona un tracciante")
     }
     gratia::appraise(models[[input$traccianti]]) & ggtitle(input$traccianti)
-  })    
-  
+  })
+
   output$summary <- renderPrint({
     if(input$traccianti == "All") {
-      return("Seleziona un tracciante") 
+      return("Seleziona un tracciante")
     }
     summary( (models[[input$traccianti]]))
   })
-  
-  # output$crossvld <- renderText({
-  #   if(input$traccianti == "All") {
-  #     return("Seleziona un tracciante")
-  #   }
-  #   crssvld[[input$traccianti]]
-  #   })
+
+  # image2 sends pre-rendered images
+  output$predict <- renderImage({
+    if (is.null(input$traccianti))
+      return(NULL)
+
+    return(list(
+      src = glue("/home/rmorelli/R/terni/png_out/{input$traccianti}_{input$res}m_{input$res}res_01.png")
+    ))
+
+  }, deleteFile = FALSE)
 
 }
 
+
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
