86250a68012ea2d130339a91acdfe52906825663
raffaele-morelli <raffaele.morelli@isprambiente.it>
2024-02-01 12:41:24 +0100

    test8

--- funzioni/f_makeSpline.R
+++ funzioni/f_makeSpline.R
@@ -8,14 +8,17 @@ makeSpline <- function(v) {
       
       # k <- filter(kappas, var == i) %>% select(kappas) %>% as.numeric()
       # log_print(sprintf("var %s kappa %s", i, k))
-      v_lineari <- c('s8_sup_200', 's6_sup_200', 'imp_200', 'bh_200', 'pop_200', 'ml_200', 'cold_area', 'hot_area', 'scrapyard', 'm_dis_ferr', 'min_d')
+      v_lineari <- c('s8_sup_200', 's6_sup_200', 
+                     'imp_200', 'bh_200', 'pop_200', 'ml_200', 
+                     'cold_area', 'hot_area', 'scrapyard', 'm_dis_ferr', 'min_d')
+      
       # v_spat <- readRDS("~/R/terni/rds_out/v_spaziali.RDS")
       
-      # if(i %in%  v_lineari) {
-        # paste0("s(", x, ", k=4)")
-      # }else{
-        # paste0("s(", x, ")")
-      # }
+      if(i %in%  v_lineari) {
+        paste0("s(", x, ", k=1)")
+      }else{
+        paste0("s(", x, ")")
+      }
       
       # case_when(
       #   k > 9 ~  paste0("s(", x, ", k=5)"),
@@ -23,7 +26,7 @@ makeSpline <- function(v) {
       #   .default = paste0("s(", x, ", k=1)")
       # )
       # paste0("s(", x, ", k=4)")
-      paste0("s(", x, ")")
+      # paste0("s(", x, ")")
     })
   )
 }
