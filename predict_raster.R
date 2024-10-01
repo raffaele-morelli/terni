764b80e4f8205bc93eb6efdac9f2989af5e932d9
raffaele-morelli <raffaele.morelli@isprambiente.it>
2024-10-01 21:58:43 +0200

    codice della predict

--- predict_raster.R
+++ predict_raster.R
@@ -42,7 +42,7 @@ map(mdf, \(x) {
   names(df)[index] <- "value"
   
   # modello di training 
-  gam_tdf <- mgcv::gam(
+  mod <- mgcv::gam(
     formula(modelli[[pltnt]]),
     data = df,
     gamma = 1.4,
@@ -51,40 +51,66 @@ map(mdf, \(x) {
 }
 
 df_raster <- do.call(rbind, tmp)
-# rbind.data.frame(tmp)
 df_sf <- st_as_sf(df_raster, coords = c("X", "Y"), crs = 32632)
 
 
-p <- predict(gam_tdf, df_sf, type="response") 
+# calcolo contributo ############
+{
+  library(mgcv)
+  intercetta <- as.numeric(mod$coefficients[1])
+  
+  Xp <- predict.gam(mod, df_sf, type = "lpmatrix")
+  jd_indxs <- grep("cold_area|hot_area", colnames(Xp))
+  
+  beta <- coef(mod)
+  b_indxs <- grep("cold_area|hot_area", names(beta))
+  
+  preds <- as.vector(Xp[,jd_indxs] %*% beta[b_indxs])
+  
+  df_l <- cbind(exp(preds + intercetta))
+  
+  names(df_l)[ncol(df_l)] <- "norm"
+  
+  # df_l <- df_l %>% mutate(date = as.Date(jd, origin = as.Date("1970-01-01")))
+  
+  acc <- Xp[,grep("cold_area|hot_area", colnames(Xp), invert = TRUE)] %*% beta[grep("cold_area|hot_area", colnames(Xp), invert = TRUE)]
+  acc <- exp(acc - intercetta)
+  
+  df_l <- cbind(df_l, acc)
+}
 
-m <- matrix(p, ncol = 109,  byrow = FALSE)
+dim(df_l)
 
-r <- raster::raster(m)
-
-bbox <- st_bbox(dominio) 
-r_extent <- c(as.numeric(bbox["xmin"]), as.numeric(bbox["xmax"]), as.numeric(bbox["ymin"]), as.numeric(bbox["ymax"]))
-
-raster::extent(r) <- r_extent
-crs(r) <- "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs"
-
-r_crop <- raster::crop(r, dominio_redux)
-
-# plot(r)
-
-r_df <- as.data.frame(r_crop, xy = TRUE) %>%
-  na.omit() %>%
-  setNames(c("x", "y", "value"))
-
-ggplot(data = r_df) +
-  geom_raster(aes(x = x, y = y, fill = value)) +
-  geom_sf(data = st_crop(strade_utm32, st_bbox(dominio_redux)), color = "grey80", fill = "transparent", size = 0.5) +
-  geom_sf(data = pt_misura_utm32, shape = 21, fill = "dodgerblue", color = "black", size = 2) +
-  geom_sf(data = filter(acciaieria, field_1 != "scrapyard"), shape = 8, fill = "dodgerblue4", color = "black", size = 2) +
-  # geom_sf_text(data = pt_misura_utm32, aes(label = Site), color = "black", nudge_x = -80, nudge_y = -125, size = 3) +
-  # scale_fill_viridis_c(direction = -1, option = "magma") +
-  scale_fill_distiller(palette = "Spectral", direction = -1, name = titolo) +
-  coord_sf(datum = sf::st_crs(32632)) +
-  # annotation_scale(location = "br", width_hint = 0.25, pad_y = unit(0.75, "in"), pad_x = unit(1, "in")) +
-  # annotation_north_arrow(location = "br", which_north = "true", pad_x = unit(1.25, "in"), pad_y = unit(1, "in"), style = north_arrow_fancy_orienteering) +
-  theme_void() +
-  theme(legend.position = "right", plot.margin = unit(c(1,1,1,1), "cm")) 
+{
+  m <- matrix(df_l[,1]+df_l[,2], ncol = 109,  byrow = FALSE)
+  
+  r <- raster::raster(m)
+  
+  bbox <- st_bbox(dominio) 
+  r_extent <- c(as.numeric(bbox["xmin"]), as.numeric(bbox["xmax"]), as.numeric(bbox["ymin"]), as.numeric(bbox["ymax"]))
+  
+  raster::extent(r) <- r_extent
+  crs(r) <- "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs"
+  
+  r_crop <- raster::crop(r, dominio_redux)
+  
+  # plot(r)
+  
+  r_df <- as.data.frame(r_crop, xy = TRUE) %>%
+    na.omit() %>%
+    setNames(c("x", "y", "value"))
+  
+  ggplot(data = r_df) +
+    geom_raster(aes(x = x, y = y, fill = value)) +
+    geom_sf(data = st_crop(strade_utm32, st_bbox(dominio_redux)), color = "grey80", fill = "transparent", size = 0.5) +
+    geom_sf(data = pt_misura_utm32, shape = 21, fill = "dodgerblue", color = "black", size = 2) +
+    geom_sf(data = filter(acciaieria, field_1 != "scrapyard"), shape = 8, fill = "dodgerblue4", color = "black", size = 2) +
+    # geom_sf_text(data = pt_misura_utm32, aes(label = Site), color = "black", nudge_x = -80, nudge_y = -125, size = 3) +
+    # scale_fill_viridis_c(direction = -1, option = "magma") +
+    scale_fill_distiller(palette = "Spectral", direction = -1, name = titolo) +
+    coord_sf(datum = sf::st_crs(32632)) +
+    # annotation_scale(location = "br", width_hint = 0.25, pad_y = unit(0.75, "in"), pad_x = unit(1, "in")) +
+    # annotation_north_arrow(location = "br", which_north = "true", pad_x = unit(1.25, "in"), pad_y = unit(1, "in"), style = north_arrow_fancy_orienteering) +
+    theme_void() +
+    theme(legend.position = "right", plot.margin = unit(c(1,1,1,1), "cm")) 
+}
